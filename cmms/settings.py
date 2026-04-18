ebc349a9eeb8ba5bde6f5a92ebeb5425c09759b3
sujay-iEng <sujay@iengaust.com.au>
2026-04-18 10:04:06 +0530

    sujay 22

--- cmms/settings.py
+++ cmms/settings.py
@@ -15,13 +15,12 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
  
- 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
- 
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-tpzqxw&&@03wq2yzgf!gzh6u2=044s2j+_!#jioe(#f^6%quzo'
- 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False  # keep false on server, true locally if needed
  
